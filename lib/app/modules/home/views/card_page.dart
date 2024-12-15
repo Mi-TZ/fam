4b3362fe6f6cf0d90716962314d82188f619d86f
Mi-TZ <jvmahajan1971@gmail.com>
2024-12-15 18:56:28 +0530

    final commit

---
+++ lib/app/modules/home/views/card_page.dart
@@ -0,0 +1,56 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/card_controller.dart';
+import '../models/card_model.dart';
+import 'widgets/card_group_widget.dart';
+
+class CardPage extends StatelessWidget {
+  final CardController _cardController = Get.put(CardController());
+
+  CardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF7F6F3),
+      body: Obx(
+        () {
+          // Loading state
+          if (_cardController.isLoading.value) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          // Error state
+          if (_cardController.error.value.isNotEmpty) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Error: ${_cardController.error.value}'),
+                  ElevatedButton(
+                    onPressed: _cardController.fetchCards,
+                    child: const Text('Retry'),
+                  )
+                ],
+              ),
+            );
+          }
+          // No cards state
+          if (_cardController.cards.isEmpty) {
+            return const Center(
+              child: Text('No cards available'),
+            );
+          }
+
+          return RefreshIndicator(
+            onRefresh: () async {
+              // Trigger the API call when pulled
+              await _cardController.fetchCards();
+            },
+            child: CardDesignFactory.createCardDesign(_cardController.cards),
+          );
+        },
+      ),
+    );
+  }
+}
